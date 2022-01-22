14fa8b050dd867d6b126151dced6dc13fb33fdcd
Marcos <marcosviniciohd@hotmail.com>
2022-01-22 11:17:32 -0300

    Classe Teste

---
+++ Teste.java
@@ -0,0 +1,9 @@
+class Teste {
+    static int i = 3;
+    public static void main(String[] a) {
+        for (new Teste().i = 10; new Teste().i < 100;
+             new Teste().i++) {
+            System.out.println(i);
+        }
+    }
+}
