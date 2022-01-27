15fb59ced1e9ffaddd8c55003e3f3e8df5150329
Marcos <marcosviniciohd@hotmail.com>
2022-01-27 16:11:14 -0300

    TestaFor

---
+++ sintaxe-variaveis-e-fluxo/src/TestaLacos.java
@@ -0,0 +1,17 @@
+
+public class TestaLacos {
+
+	public static void main(String[] args) {
+
+		
+		for (int multiplicador = 1; multiplicador <= 10; multiplicador++) {
+			for(int contador = 1; contador <= 10; contador++) {
+				System.out.println(multiplicador + " X " + contador + " = " + multiplicador * contador);
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
