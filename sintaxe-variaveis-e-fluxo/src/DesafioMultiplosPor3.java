75fd0ea05ba3b90c837801e72abee9893f6d0824
Marcos <marcosviniciohd@hotmail.com>
2022-01-28 15:16:47 -0300

    Desafio múltiplos de 3

---
+++ sintaxe-variaveis-e-fluxo/src/DesafioMultiplosPor3.java
@@ -0,0 +1,11 @@
+public class DesafioMultiplosPor3 {
+	
+	public static void main(String[] args) {
+		for(int numero = 1; numero <= 100; numero++) {
+			if (numero % 3 == 0) {
+				System.out.print(numero + " ");
+			}
+		}
+	}
+
+}
